7a5151a13a9bda44972eba0e3eca6d73b75fb97d
Rasha <r.alamleh@gmail.com>
2020-11-03 15:52:51 +0200

    Never gonna let you down

--- server.js
+++ server.js
@@ -1,14 +1,23 @@
 
 //import the express and path packages
+const http = require('http');
 const express = require("express");
 const path = require("path");
 // create an instance of an Express application, which you then store in app.
-//define the port the server will use to listen for requests.
 const app = express();
-const port = process.env.PORT || "8000";
+app.use(express.json());
+app.use(express.static("client"));
+
+app.use("/", function(req,res){
+    res.sendFile(path.join(__dirname+'/client/index.html'));
+    //__dirname : It will resolve to your project folder.
+  });
+//define the port the server will use to listen for requests.
+const port = 3000;
+
 //create a simple route handler for the HTTP GET / request that replies with a string.
 app.get("/", (req, res) => {
-    res.status(200).send("WHATABYTE: Food For Devs");
+    res.status(200).send("CSV Report Generator");
 });
 //start a server listening for incoming requests on port 
 //and display a message to confirm it's listening.
